6b7d3e13a6a512242bd8fbb56fc2612bdaac183f
AbdUlwahab Bitar <aboodbitarayash@gmail.com>
2024-05-23 15:03:42 +0300

    description

--- lib/screens/blog_screen.dart
+++ lib/screens/blog_screen.dart
@@ -1,15 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter_jobs_app/widgets/archives_widget.dart';
+import 'package:flutter_jobs_app/widgets/blog_widget.dart';
+import 'package:flutter_jobs_app/widgets/questions_widgets.dart';
 
-import 'package:flutter_jobs_app/contents.dart';
-import 'package:flutter_jobs_app/widgets/custom_appBar.dart';
-import 'package:flutter_jobs_app/widgets/custom_floating_button.dart';
-
-class BlogScreen extends StatelessWidget {
+class BlogScreen extends StatefulWidget {
   BlogScreen({super.key});
   static String id = 'BlogScreen';
+
+  @override
+  State<BlogScreen> createState() => _BlogScreenState();
+}
+
+class _BlogScreenState extends State<BlogScreen> {
   String selectedSection = 'Blog';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,108 +56,36 @@ class BlogScreen extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
                       customContainer(
-                        section: 'Blog',
-                        selectedSection: selectedSection,
-                      ),
+                          section: 'Blog',
+                          onTap: () {
+                            setState(() {
+                              selectedSection = 'Blog';
+                            });
+                          }),
                       customContainer(
-                        section: 'Questions',
-                        selectedSection: selectedSection,
-                      ),
+                          section: 'Questions',
+                          onTap: () {
+                            setState(() {
+                              selectedSection = 'Questions';
+                            });
+                          }),
                       customContainer(
-                        section: 'Archives',
-                        selectedSection: selectedSection,
-                      ),
+                          section: 'Archives',
+                          onTap: () {
+                            setState(() {
+                              selectedSection = 'Archives';
+                            });
+                          }),
                     ],
                   ),
                   SizedBox(
                     height: 10,
                   ),
-                  Stack(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.all(8),
-                        height: MediaQuery.of(context).size.height * 0.8,
-                        color: Colors.amber,
-                        child: ListView.builder(
-                          itemBuilder: (context, index) {
-                            return ListTile(
-                              title: Row(
-                                children: [
-                                  Image.asset(
-                                    'images/photo.png',
-                                    height: 40,
-                                    width: 40,
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  Text(
-                                    'Yuna',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              isThreeLine: true,
-                              subtitle: Padding(
-                                padding:
-                                    const EdgeInsets.only(top: 8.0, left: 6),
-                                child: Column(
-                                  children: [
-                                    Text(
-                                      'Sleep in Sunday is the best day of the week! #sunspot',
-                                      style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w400,
-                                      ),
-                                    ),
-                                    Row(
-                                      children: [
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.favorite_border_rounded,
-                                          ),
-                                        ),
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.chat_bubble_outline,
-                                          ),
-                                        ),
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(
-                                            Icons.bookmark_border,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              dense: false,
-                            );
-                          },
-                          itemCount: 2,
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.only(
-                            top: MediaQuery.of(context).size.height * 0.62,
-                            right: 24),
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: CustomFloatingButton(
-                            icon: Icons.add,
-                            function: () {},
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                  selectedSection == 'Blog'
+                      ? BlogWidget()
+                      : selectedSection == 'Questions'
+                          ? QuestionsWidget()
+                          : ArchivesWidget(),
                 ],
               ),
             ],
@@ -162,12 +95,9 @@ class BlogScreen extends StatelessWidget {
     );
   }
 
-  Widget customContainer(
-      {required String section, required String selectedSection}) {
+  Widget customContainer({required String section, required void onTap()}) {
     return InkWell(
-      onTap: () {
-        selectedSection == section;
-      },
+      onTap: onTap,
       child: Container(
         padding: EdgeInsets.symmetric(
           horizontal: 16,
---
+++ lib/widgets/questions_widgets.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_jobs_app/widgets/custom_floating_button.dart';
+
+class QuestionsWidget extends StatelessWidget {
+  const QuestionsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.all(8),
+          height: MediaQuery.of(context).size.height * 0.8,
+          color: Colors.pink,
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Row(
+                  children: [
+                    Image.asset(
+                      'images/photo.png',
+                      height: 40,
+                      width: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'Yuna',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                isThreeLine: true,
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 8.0, left: 6),
+                  child: Column(
+                    children: [
+                      Text(
+                        'Sleep in Sunday is the best day of the week! #sunspot',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.favorite_border_rounded,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.chat_bubble_outline,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.bookmark_border,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                dense: false,
+              );
+            },
+            itemCount: 2,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.62, right: 24),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: CustomFloatingButton(
+              icon: Icons.add,
+              function: () {},
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
